d6b06b069848834988c90a72fb84cfab875c953f
EmileMusoni <118208536+EmileMusoni@users.noreply.github.com>
2022-11-26 18:19:54 +0200

    Add files via upload

---
+++ MyLab3/app/src/main/java/com/example/book/model/SampleBookData.kt
@@ -0,0 +1,9 @@
+package com.example.book.model
+
+val sampleBookData = listOf(
+    Book("1", "Behold the Dreamers"),
+    Book("2", "Horse"),
+    Book("3", "A Girl is a Body of Water"),
+    Book("4", "Once We Were Brothers"),
+    Book("5", "Olga Dies Dreaming")
+)
