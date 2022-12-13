de1c9fcdbcb1df26de3a3abdb5b1f27a1e7279f4
EmileMusoni <118208536+EmileMusoni@users.noreply.github.com>
2022-12-13 08:41:27 +0200

    Delete Project_Android-iteration2/Project_Android-iteration2 directory

--- Project_Android-iteration2/Project_Android-iteration2/app/src/main/java/com/example/mangrove/Model/product.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mangrove.Model
-
-data class product(val id: String, val prod: String,val kgPerSack: String,val sacks: String)
-{
-
-}
--- Project_Android-iteration2/Project_Android-iteration2/app/src/main/java/com/example/mangrove/compose/ProductModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mangrove.compose
-
- data class ProductModel(
-var productPhone: String,
-var productName: String,
-var productKg: String,
-var programSacks: String?
-)
