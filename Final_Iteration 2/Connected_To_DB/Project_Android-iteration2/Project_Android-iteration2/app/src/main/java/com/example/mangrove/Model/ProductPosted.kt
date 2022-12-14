7ccd4787818a5d4cc405dcc86597c7fe163a9553
EmileMusoni <118208536+EmileMusoni@users.noreply.github.com>
2022-12-14 13:12:31 +0200

    Delete Final_Iteration 2/Connected_To_DB/Project_Android-iteration2 directory

--- Final_Iteration 2/Connected_To_DB/Project_Android-iteration2/Project_Android-iteration2/app/src/main/java/com/example/mangrove/Model/ProductPosted.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mangrove.Model
-
-data class ProductPosted(val name: String,val location: String,val productType: String,
-                         val number:String,val kiloPersack:String,val numbsack: String)
-{
-
-}
--- Final_Iteration 2/Connected_To_DB/Project_Android-iteration2/Project_Android-iteration2/app/src/main/java/com/example/mangrove/NavigationItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mangrove
-
-sealed class NavigationItem(var route: String,var icon: Int, var name: String )
-{
-    object  Home : NavigationItem("Home", R.drawable.ic_baseline_home_24,"Home")
-    object  Search : NavigationItem("search", R.drawable.ic_baseline_search_24,"Search")
-    object  Post : NavigationItem("Post", R.drawable.ic_baseline_post_add_24,"Post")
-    object  Profile : NavigationItem("profile", R.drawable.ic_baseline_person_24,"Profile")
-}
