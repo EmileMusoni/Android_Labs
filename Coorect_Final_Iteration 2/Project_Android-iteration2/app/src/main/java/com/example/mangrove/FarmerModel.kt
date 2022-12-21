addd8831dd188a68589de2b6462be0632d8e30d7
EmileMusoni <118208536+EmileMusoni@users.noreply.github.com>
2022-12-21 14:04:29 +0200

    Add files via upload

---
+++ Coorect_Final_Iteration 2/Project_Android-iteration2/app/src/main/java/com/example/mangrove/FarmerModel.kt
@@ -0,0 +1,12 @@
+package com.example.mangrove
+
+
+data class FarmerModel(
+ var FarmerName: String,
+var FarmerPhone: String,
+var FarmerLocation: String,
+ var FarmerEmail: String?,
+var FarmerPassword: String,
+ var ConfirmPassword: String
+)
+
---
+++ Coorect_Final_Iteration 2/Project_Android-iteration2/app/src/main/java/com/example/mangrove/Model/product.kt
@@ -0,0 +1,6 @@
+package com.example.mangrove.Model
+
+data class product(val id: String, val prod: String,val kgPerSack: String,val sacks: String)
+{
+
+}
